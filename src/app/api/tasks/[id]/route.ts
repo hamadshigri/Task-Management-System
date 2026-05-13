478756422287e34672c380ac3d89dfcab2bc91e1
Hamad Shigri <hamad.shigri95@gmail.com>
2026-05-13 12:10:06 +0500

    Changing Route params code

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,18 +1,13 @@
 export const runtime = "nodejs";
+
 import { connectDB } from "@/lib/db";
 import { getUserFromToken } from "@/lib/auth";
 import Task from "@/models/Task";
 
-type Params = {
-  params: {
-    id: string;
-  };
-};
-
 // UPDATE TASK
 export async function PUT(
   req: Request,
-  { params }: Params
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     await connectDB();
@@ -20,25 +15,21 @@ export async function PUT(
     const user = await getUserFromToken();
 
     if (!user) {
-      return Response.json(
-        { error: "Unauthorized" },
-        { status: 401 }
-      );
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const { id } = params;    
+    const { id } = await params;
 
     const body = await req.json();
 
-    const updatedTask =
-      await Task.findOneAndUpdate(
-        {
-          _id: id,
-          userId: user.userId,
-        },
-        body,
-        { new: true }
-      );
+    const updatedTask = await Task.findOneAndUpdate(
+      {
+        _id: id,
+        userId: user.userId,
+      },
+      body,
+      { new: true }
+    );
 
     return Response.json(updatedTask);
   } catch (error) {
@@ -54,7 +45,7 @@ export async function PUT(
 // DELETE TASK
 export async function DELETE(
   req: Request,
-  { params }: Params
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     await connectDB();
@@ -62,22 +53,17 @@ export async function DELETE(
     const user = await getUserFromToken();
 
     if (!user) {
-      return Response.json(
-        { error: "Unauthorized" },
-        { status: 401 }
-      );
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const { id } = params;
+    const { id } = await params;
 
     await Task.findOneAndDelete({
       _id: id,
       userId: user.userId,
     });
 
-    return Response.json({
-      message: "Task deleted",
-    });
+    return Response.json({ message: "Task deleted" });
   } catch (error) {
     console.log(error);
 
