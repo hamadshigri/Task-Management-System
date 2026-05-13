03876cc6266cb3ec2928dda2489d7534c90ec3dd
Hamad Shigri <hamad.shigri95@gmail.com>
2026-05-13 11:15:24 +0500

    cookies fixed

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -27,8 +27,8 @@ export async function POST(req: Request) {
 
   (await cookies()).set("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     path: "/",
   });
 
